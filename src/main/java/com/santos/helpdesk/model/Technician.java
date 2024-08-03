b1504e95819a71a8a3cb279c416526dcc77f2598
Bruno Santos <bruno_st17@hotmail.com>
2024-08-02 23:36:16 -0300

    fix: ajuste da model de tecnico

--- src/main/java/com/santos/helpdesk/model/Technician.java
+++ src/main/java/com/santos/helpdesk/model/Technician.java
@@ -1,5 +1,6 @@
 package com.santos.helpdesk.model;
 
+import com.santos.helpdesk.enums.Profile;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -7,15 +8,21 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 public class Technician extends Person {
     @OneToMany(mappedBy = "technician")
     private List<Called> called = new ArrayList<>();
+
+    public Technician(Long id, String name, String cpf, String email, String password) {
+        super(id, name, cpf, email, password);
+        addProfile(Profile.CLIENT);
+    }
 }
