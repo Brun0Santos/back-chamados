ba5551894f0c0fb2bcbf93469722b61b62719971
Bruno Santos <bruno_st17@hotmail.com>
2024-08-04 09:54:14 -0300

    feat adicionando novas rotas na controller

--- src/main/java/com/santos/helpdesk/controller/TechnicianController.java
+++ src/main/java/com/santos/helpdesk/controller/TechnicianController.java
@@ -2,12 +2,13 @@ package com.santos.helpdesk.controller;
 
 import com.santos.helpdesk.dto.TechnicianDto;
 import com.santos.helpdesk.service.TechnicianService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/technician")
@@ -19,5 +20,27 @@ public class TechnicianController {
     public ResponseEntity<TechnicianDto> findById(@PathVariable long id) {
         System.out.println(id);
         return ResponseEntity.ok().body(technicianService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TechnicianDto>> findAll() {
+        return ResponseEntity.ok().body(technicianService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody @Valid TechnicianDto technicianDto) {
+        technicianService.save(technicianDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TechnicianDto> update(@PathVariable Long id, @RequestBody TechnicianDto request) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(technicianService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        technicianService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
