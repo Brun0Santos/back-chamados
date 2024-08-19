359de1b068f33f6eb466437fce79b9e6d026631d
Bruno Santos <bruno_st17@hotmail.com>
2024-08-19 20:47:57 -0300

    feat: adicionando a repository de cliente

--- src/main/java/com/santos/helpdesk/repository/ClientRepository.java
+++ src/main/java/com/santos/helpdesk/repository/ClientRepository.java
@@ -1,7 +1,13 @@
 package com.santos.helpdesk.repository;
 
 import com.santos.helpdesk.model.Client;
+import com.santos.helpdesk.model.Technician;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Technician> findByCpf(String email);
+
+    Optional<Technician> findByEmail(String email);
 }
