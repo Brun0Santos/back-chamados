dc1fb1167a1ded289322e04b7cfe564e3eece0b7
Bruno Santos <bruno_st17@hotmail.com>
2024-08-30 22:46:45 -0300

    fix: adicao da entidade client

--- src/main/java/com/santos/helpdesk/model/Client.java
+++ src/main/java/com/santos/helpdesk/model/Client.java
@@ -1,21 +1,48 @@
 package com.santos.helpdesk.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.santos.helpdesk.enums.Profile;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-public class Client extends Person {
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private Set<Profile> profiles = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate creationDate = LocalDate.now();
+
     @OneToMany(mappedBy = "client")
     private List<Called> called = new ArrayList<>();
+
+    @PostConstruct
+    public void initializeProfile() {
+        this.profiles.add(Profile.CLIENT);
+    }
 }
